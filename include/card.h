32e3e39b1ba65241d5abf6a4524a41b9b815281d
owodunniPC <alex.o.poole@gmail.com>
2014-08-27 21:23:07 +0200

    Added header

--- include/card.h
+++ include/card.h
@@ -1,3 +1,20 @@
+//#########################################      
+//#                                       #                     
+//#       -----BEATING POKER------        # 
+//#       - ~a Poole production~ -        #
+//#       ------------------------        #
+//#                                       #
+//#########################################
+//# filename:	card.h                    #
+//# creator:	Alexander Poole                 #
+//# date: 		2014.08.27            #
+//#                                       #
+//# The .h file for the card class                 #
+//#########################################
+//#
+//# TODO:
+//#
+
 #ifndef __CARD_H_INCLUDE__
 #define __CARD_H_INCLUDE__
 
--- include/main.h
+++ include/main.h
@@ -1,3 +1,20 @@
+//#########################################      
+//#                                       #                     
+//#       -----BEATING POKER------        # 
+//#       - ~a Poole production~ -        #
+//#       ------------------------        #
+//#                                       #
+//#########################################
+//# filename:	main.h                    #
+//# creator:	Alexander Poole                 #
+//# date: 		2014.08.27            #
+//#                                       #
+//# The .h file for the main file                 #
+//#########################################
+//#
+//# TODO:
+//#
+
 #ifndef __MAIN_H_INCLUDED__
 #define __MAIN_H_INCLUDED__
 
