fe062562eaf649d0c78384c3747967a3058a48c3
alexander poole <alex.o.poole@gmail.com>
2014-10-29 16:11:09 +0100

    minor changes

--- src/main.cc
+++ src/main.cc
@@ -37,7 +37,7 @@ main()
     Gameboard myBoard();
 
     myBoard().SeatPlayer(&player1,2);
-    if(myBoard.SeatPlayer(&player2,2)){
+    if(myBoard().SeatPlayer(&player2,2)){
     cout<<"ERROR\n";
     }
     else
