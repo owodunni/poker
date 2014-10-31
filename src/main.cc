b6563e6942c15682d18447d5eacb29c5d4b03f1a
alexander poole <alex.o.poole@gmail.com>
2014-10-31 09:30:48 +0100

    testing gameboard

--- src/main.cc
+++ src/main.cc
@@ -37,10 +37,10 @@ main()
     Gameboard myBoard;
 
     myBoard.SeatPlayer(&player1,8);
-    myBoard.RemovePlayer(&player1);
+    /*myBoard.RemovePlayer(&player1);
     if(myBoard.SeatPlayer(&player2,2)){
     cout<<"Funkar\n";
-    }
+    }*/
     
 	return 0;
 }
