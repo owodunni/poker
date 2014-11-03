93e50c495c25c3f2ffca51212b9615a91973cb90
owodunniPC <alex.o.poole@gmail.com>
2014-11-03 20:23:15 +0100

    mincha

--- include/test.h
+++ include/test.h
@@ -25,47 +25,7 @@ using namespace std;
 /**
 	* Creating one of each class and destroying them
 	*/
-
-void TEST_CLASSES(){
-
-suit_t newSuit HEARTS;
-
-Card card;
-cout<<"Deleting card\n";
-delete card;
-
-Card cardA(2, newSuit);
-cout<<"Deleting cardA\n";
-delete cardA;
-
-Deck deck;
-cout<<"Deleting card\n";
-delete deck;
-
-Gameboard gameboard;
-cout<<"Deleting card\n";
-delete gameboard;
-
-Gameboard gameboardA();
-Player player1();
-Player player2();
-gameboardA.SeatPlayer(&player1, 2);
-gameboardA.SeatPlayer(&player2, 3);
-cout<<"Deleting card\n";
-delete gameboardA;
-
-Player player;
-cout<<"Deleting card\n";
-delete player;
-
-Player playerA();
-Card card1():
-Card card2();
-playerA.card1(card1,1);
-playerA.card2(card2,2);
-cout<<"Deleting card\n";
-delete playerA;
-}
+void TEST_CLASSES();
 
 
 #endif
